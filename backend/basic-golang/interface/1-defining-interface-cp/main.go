8825e2d6c03661c170dd60629aa512e65e3eff30
althafariq <althafariq04@gmail.com>
2022-03-29 22:47:26 +0700

    cp error handling, interface & package

--- backend/basic-golang/interface/1-defining-interface-cp/main.go
+++ backend/basic-golang/interface/1-defining-interface-cp/main.go
@@ -12,13 +12,58 @@ import "fmt"
 // Bonus untuk JuniorEngineer adalah 1 * BaseSalary
 
 // TODO: answer here
+type Employee interface {
+	GetBonus() int
+}
+
+type Manager struct {
+	BaseSalary int
+}
+
+func (m Manager) GetBonus() int {
+	return m.BaseSalary * 3
+}
+
+type SeniorEngineer struct {
+	BaseSalary int
+}
+
+func (m SeniorEngineer) GetBonus() int {
+	return m.BaseSalary * 2
+}
+
+type JuniorEngineer struct {
+	BaseSalary int
+}
+
+func (m JuniorEngineer) GetBonus() int {
+	return m.BaseSalary
+}
 
 func TotalEmployeeBonus(employees []Employee) int {
 	// Hitunglah total bonus yang dikeluarkan dari list of Employee
 	// TODO: answer here
+	total := 0
+	for _, employee := range employees {
+		total += employee.GetBonus()
+	}
+
+	return total
 }
 
 func main() {
 	// Buatlah objek konkret untuk masing-masing objek dan panggil function TotalEmployeeBonus. Print total bonus untuk semua employee.
 	// TODO: answer here
+	manager := Manager{
+		BaseSalary: 20000000,
+	}
+	seniorEngineer := SeniorEngineer{
+		BaseSalary: 15000000,
+	}
+	juniorEngineer := JuniorEngineer{
+		BaseSalary: 10000000,
+	}
+
+	totalBonus := TotalEmployeeBonus([]Employee{manager, seniorEngineer, juniorEngineer})
+	fmt.Println("total bonus", totalBonus)
 }
