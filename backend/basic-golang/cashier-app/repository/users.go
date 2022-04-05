0f372324b3eb0ee9fc4d48095e111f75fb1eb75e
althafariq <althafariq04@gmail.com>
2022-04-06 00:33:51 +0700

    still working on it

--- backend/basic-golang/cashier-app/repository/users.go
+++ backend/basic-golang/cashier-app/repository/users.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/ruang-guru/playground/backend/basic-golang/cashier-app/db"
 )
@@ -17,20 +18,29 @@ func NewUserRepository(db db.DB) UserRepository {
 func (u *UserRepository) LoadOrCreate() ([]User, error) {
 	records, err := u.db.Load("users")
 	if err != nil {
-		return nil, err
+		records = [][]string{
+			{"username", "password", "loggedin"},
+		}
+		if err := u.db.Save("users", records); err != nil {
+			return nil, err
+		}
 	}
 
 	result := make([]User, 0)
 	for i := 1; i < len(records); i++ {
-		result = append(result, User{
+		loggedin, err := strconv.ParseBool(records[i][2])
+		if err != nil {
+			return nil, err
+		}
+
+		user := User{
 			Username: records[i][0],
 			Password: records[i][1],
-			Loggedin: false,
-		})
+			Loggedin: loggedin,
+		}
+		result = append(result, user)
 	}
-
 	return result, nil
-
 }
 
 func (u *UserRepository) SelectAll() ([]User, error) {
@@ -44,37 +54,34 @@ func (u *UserRepository) SelectAll() ([]User, error) {
 
 func (u UserRepository) Login(username string, password string) (*string, error) {
 	u.LogoutAll()
+
 	users, err := u.SelectAll()
 	if err != nil {
 		return nil, err
 	}
 
-	for i := 0; i < len(users); i++ {
-		if users[i].Username == username && users[i].Password == password {
-			err := u.changeStatus(username, true)
-			if err != nil {
-				return nil, err
-			}
-			return &users[i].Username, nil
+	//loop through users and return username if logged in
+	for _, user := range users {
+		if user.Username == username && user.Password == password {
+			u.changeStatus(username, true)
+			return &username, nil
 		}
 	}
-
 	return nil, fmt.Errorf("Login Failed") // TODO: replace this
 }
 
 func (u *UserRepository) FindLoggedinUser() (*string, error) {
-	records, err := u.SelectAll()
+	users, err := u.SelectAll()
 	if err != nil {
 		return nil, err
 	}
 
-	//loop through users
-	for i := 0; i < len(records); i++ {
-		if records[i].Loggedin {
-			return &records[i].Username, nil
+	//loop through users and return username if logged in
+	for i := 0; i < len(users); i++ {
+		if users[i].Loggedin {
+			return &users[i].Username, nil
 		}
 	}
-
 	return nil, fmt.Errorf("no user is logged in") // TODO: replace this
 }
 
@@ -84,12 +91,11 @@ func (u *UserRepository) Logout(username string) error {
 		return err
 	}
 	//if username is logged in, call changeStatus to false
-	if *records == username {
-		err := u.changeStatus(username, false)
-		if err != nil {
-			return err
-		}
+	err = u.changeStatus(*records, false)
+	if err != nil {
+		return err
 	}
+
 	return nil // TODO: replace this
 }
 
@@ -102,6 +108,7 @@ func (u *UserRepository) Save(users []User) error {
 		records = append(records, []string{
 			users[i].Username,
 			users[i].Password,
+			strconv.FormatBool(users[i].Loggedin),
 		})
 	}
 	return u.db.Save("users", records)
