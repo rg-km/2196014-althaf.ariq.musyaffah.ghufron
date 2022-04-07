17588419d05dab39751e738c41d2bcd3005ce423
althafariq <althafariq04@gmail.com>
2022-04-07 14:59:21 +0700

    cp map

--- backend/basic-golang/map/1-map-basic-cp/main.go
+++ backend/basic-golang/map/1-map-basic-cp/main.go
@@ -6,4 +6,15 @@ import "fmt"
 // Output satu semua key dan value yang ada di map tersebut
 func main() {
 	// TODO: answer here
+	var provinsi = map[string]string{
+		"Kalimantan Barat":   "Pontianak",
+		"Kalimantan Tengah":  "Palangkaraya",
+		"Kalimantan Selatan": "Samarinda",
+		"Kalimantan Timur":   "Balikpapan",
+		"Kalimantan Utara":   "Tanjung Selor",
+	}
+
+	for k, v := range provinsi {
+		fmt.Println(k, ":", v)
+	}
 }
