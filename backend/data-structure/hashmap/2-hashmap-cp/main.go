f030dbc791f43d1826cdfcd549484a4a27b8122d
althafariq <althafariq04@gmail.com>
2022-04-12 17:50:51 +0800

    cp hashmap

--- backend/data-structure/hashmap/2-hashmap-cp/main.go
+++ backend/data-structure/hashmap/2-hashmap-cp/main.go
@@ -21,11 +21,71 @@ package main
 import "fmt"
 
 func main() {
-	var str1 = "fried"
-	var str2 = "fired"
+	var str1 = "keen"
+	var str2 = "knee"
 	fmt.Println(AnagramsChecker(str1, str2))
 }
 
 func AnagramsChecker(str1 string, str2 string) string {
-	return "" // TODO: replace this
+
+	if len(str1) != len(str2) {
+		return "Bukan Anagram"
+	}
+
+	strMap := make(map[string]int)
+
+	for _, val := range str1 {
+		x := strMap[string(val)]
+
+		if x == 0 {
+			strMap[string(val)] = 1
+		} else {
+			strMap[string(val)] = x + 1
+		}
+	}
+
+	for _, val := range str2 {
+		x := strMap[string(val)]
+
+		if x == 0 {
+			strMap[string(val)] = 1
+		} else {
+			strMap[string(val)] = x + 1
+		}
+	}
+
+	isAnagram := true
+
+	fmt.Println(strMap, isAnagram)
+
+	for _, val := range strMap {
+		if val%2 != 0 {
+			isAnagram = false
+			break
+		}
+	}
+
+	if isAnagram {
+		return "Anagram"
+	}
+	return "Bukan Anagram"
+
+	// str1Map := map[rune]int{}
+	// str2Map := map[rune]int{}
+
+	// for _, val := range str1 {
+	// 	str1Map[val]++
+	// }
+
+	// for _, val := range str2 {
+	// 	str2Map[val]++
+	// }
+
+	// for key, val := range str1Map {
+	// 	if str2Map[key] != val {
+	// 		return "Bukan Anagram"
+	// 	}
+	// }
+
+	// return "Anagram"
 }
