083a75b4944f01f104dcd55af592178ed1fd6182
althafariq <althafariq04@gmail.com>
2022-04-14 19:32:07 +0800

    cp stack

--- backend/data-structure/stack/3-pop-cp/main.go
+++ backend/data-structure/stack/3-pop-cp/main.go
@@ -8,16 +8,38 @@ var ErrStackUnderflow = errors.New("stack underflow")
 
 type Stack struct {
 	// TODO: answer here
+	Top  int
+	Data []int
+	Size int
 }
 
 func NewStack(size int) Stack {
-	// TODO: answer here
+	return Stack{
+		Top:  -1,
+		Data: []int{},
+		Size: size,
+	}
 }
 
 func (s *Stack) Push(Elemen int) error {
-	// TODO: answer here
+	//validate if len of data is equal to size means stack is full
+	if len(s.Data) == s.Size {
+		return errors.New("stack overflow")
+	} else {
+		s.Top += 1
+		s.Data = append(s.Data, Elemen)
+	}
+	return nil
 }
 
 func (s *Stack) Pop() (int, error) {
-	// TODO: answer here
+	//check if stack is empty
+	if s.Top == -1 {
+		return 0, ErrStackUnderflow
+	} else {
+		poppedValue := s.Data[s.Top] //get value of top
+		s.Top -= 1
+		s.Data = s.Data[:len(s.Data)-1] //remove top
+		return poppedValue, nil
+	}
 }
--- backend/data-structure/stack/4-peek-cp/main.go
+++ backend/data-structure/stack/4-peek-cp/main.go
@@ -8,16 +8,38 @@ var ErrEmptyStack = errors.New("stack is empty")
 
 type Stack struct {
 	// TODO: answer here
+	Top  int
+	Data []int
+	Size int
 }
 
 func NewStack(size int) Stack {
 	// TODO: answer here
+	return Stack{
+		Top:  -1,
+		Data: []int{},
+		Size: size,
+	}
 }
 
 func (s *Stack) Push(Elemen int) error {
 	// TODO: answer here
+	//validate if len of data is equal to size means stack is full
+	if len(s.Data) == s.Size {
+		return errors.New("stack overflow")
+	} else {
+		s.Top += 1
+		s.Data = append(s.Data, Elemen)
+	}
+	return nil
 }
 
 func (s *Stack) Peek() (int, error) {
 	// TODO: answer here
+	//check if stack is empty
+	if s.Top == -1 {
+		return 0, ErrEmptyStack
+	} else {
+		return s.Data[s.Top], nil
+	}
 }
