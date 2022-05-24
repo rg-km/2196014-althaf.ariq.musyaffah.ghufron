5fac99b99cb04baeda0079e544326ed107cbbeeb
althafariq <althafariq04@gmail.com>
2022-05-24 18:19:25 +0700

    cp net http

---
+++ backend/basic-golang/net-http/3-server-web-api-cp/main_test.go
@@ -0,0 +1,12 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Test", func() {
+	It("should pass", func() {
+		Expect(true).To(BeTrue())
+	})
+})
